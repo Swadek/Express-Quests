84ab375648b1e12d84918329fc0088c472d3f178
Antoine JULIAN <antoine.julian@hotmail.com>
2023-05-24 17:00:51 +0200

    Express02

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 const express = require("express");
-
+require("dotenv").config();
 const app = express();
-
-const port = 5000;
-
-const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
-};
-
-app.get("/", welcome);
-
+const port = process.env.APP_PORT ?? 5000;
 const movieHandlers = require("./movieHandlers");
 
+app.get("/", movieHandlers.welcome);
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", movieHandlers.getUsers);
+app.get("/api/users/:id", movieHandlers.getUserById);
 
 app.listen(port, (err) => {
   if (err) {
--- movieHandlers.js
+++ movieHandlers.js
@@ -25,23 +25,78 @@ const movies = [
   },
 ];
 
+const database = require("./database");
+
+const welcome = (req, res) => {
+  res.send("Welcome to my favourite movie list");
+};
+
 const getMovies = (req, res) => {
-  res.json(movies);
+  database
+    .query("select * from movies")
+    .then(([movies]) => {
+      res.status(200).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
 };
 
 const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
 
-  const movie = movies.find((movie) => movie.id === id);
+  database
+    .query("select * from movies where id = ?", [id])
+    .then(([movies]) => {
+      if (movies[0] != null) {
+        res.status(200).json(movies[0]);
+      } else {
+        res.status(404).send("Not Found");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
 
-  if (movie != null) {
-    res.json(movie);
-  } else {
-    res.status(404).send("Not Found");
-  }
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+const getUsers = (req, res) => {
+  database
+    .query("select * from users")
+    .then(([users]) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+const getUserById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("select * from users where id = ?", [id])
+    .then(([users]) => {
+      if (users[0] != null) {
+        res.status(200).json(users[0]);
+      } else {
+        res.status(404).send("Not Found");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+
+      res.status(500).send("Error retrieving data from database");
+    });
 };
 
 module.exports = {
+  getUsers,
+  getUserById,
+  welcome,
   getMovies,
   getMovieById,
 };
