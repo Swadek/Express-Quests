38d1cb96a6356f7caad7800213349806665f9239
Antoine JULIAN <antoine.julian@hotmail.com>
2023-05-25 16:33:59 +0200

    Express03 OK

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
-require("dotenv").config();
 const app = express();
+app.use(express.json());
+require("dotenv").config();
 const port = process.env.APP_PORT ?? 5000;
 const movieHandlers = require("./movieHandlers");
 
@@ -9,6 +10,9 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", movieHandlers.getUsers);
 app.get("/api/users/:id", movieHandlers.getUserById);
+
+app.post("/api/movies", movieHandlers.postMovie);
+app.post("/api/users", movieHandlers.postUser);
 
 app.listen(port, (err) => {
   if (err) {
