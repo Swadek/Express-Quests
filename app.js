37bb70c37a1fb2951244f64d63f3db24bc50690a
Antoine JULIAN <antoine.julian@hotmail.com>
2023-06-15 14:42:32 +0200

    Express08 OK

--- app.js
+++ app.js
@@ -2,31 +2,38 @@ require("dotenv").config();
 
 const express = require("express");
 
-
 const app = express();
 
 app.use(express.json());
 
 const port = process.env.APP_PORT ?? process.env.APP_PORT ?? 5000;
+const userHandlers = require("./userHandlers");
 const movieHandlers = require("./movieHandlers");
+
+const {
+  hashPassword,
+  getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword,
+  verifyToken,
+  verifyTokenId,
+} = require("./auth");
 
 app.get("/", movieHandlers.welcome);
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+app.post("/api/login", getUserByEmailWithPasswordAndPassToNext, verifyPassword);
+app.post("/api/users", hashPassword, userHandlers.postUser);
+
+app.use(verifyToken); // authentication wall : verifyToken is activated for each route after this line
+
 app.post("/api/movies", movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
-const userHandlers = require("./userHandlers");
-const { hashPassword, getUserByEmailWithPasswordAndPassToNext, verifyPassword } = require("./auth");
-
-app.get("/api/users", userHandlers.getUsers);
-app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", hashPassword, userHandlers.postUser);
-app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
-app.delete("/api/users/:id", userHandlers.deleteUser);
-
-app.post("/api/login", getUserByEmailWithPasswordAndPassToNext);
+app.put("/api/users/:id", verifyTokenId, hashPassword, userHandlers.updateUser);
+app.delete("/api/users/:id", verifyTokenId, userHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
--- auth.js
+++ auth.js
@@ -1,5 +1,6 @@
 const argon2 = require("@node-rs/argon2");
 const database = require("./database");
+const jwt = require("jsonwebtoken");
 
 const hashingOptions = {
   type: argon2.argon2id,
@@ -23,7 +24,7 @@ const hashPassword = (req, res, next) => {
 };
 
 const getUserByEmailWithPasswordAndPassToNext = (req, res, next) => {
-  const { email } = req.body
+  const { email } = req.body;
   database
     .query("select * from users where email = ?", [email])
     .then(([users]) => {
@@ -41,7 +42,56 @@ const getUserByEmailWithPasswordAndPassToNext = (req, res, next) => {
 };
 
 const verifyPassword = (req, res) => {
-  res.send(req.user);
-}
+  argon2
+    .verify(req.user.hashedPassword, req.body.password)
+    .then((isVerified) => {
+      if (isVerified) {
+        const payload = { sub: req.user.id };
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+          expiresIn: "3600000",
+        });
+        delete req.user.hashedPassword;
+        res.send({ token, user: req.user });
+      } else {
+        res.sendStatus(401);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
 
-module.exports = { hashPassword, getUserByEmailWithPasswordAndPassToNext, verifyPassword };
+const verifyToken = (req, res, next) => {
+  try {
+    const authorizationHeader = req.get("Authorization");
+    if (authorizationHeader == null) {
+      throw new Error("Authorization header is missing");
+    }
+    const [type, token] = authorizationHeader.split(" ");
+    if (type !== "Bearer") {
+      throw new Error("Authorization header has not the 'Bearer' type");
+    }
+    req.payload = jwt.verify(token, process.env.JWT_SECRET);
+    next();
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(403);
+  }
+};
+
+const verifyTokenId = (req, res, next) => {
+  console.log(req.payload.sub, req.params, req.query);
+  if (req.payload.sub == req.params.id) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+};
+module.exports = {
+  hashPassword,
+  getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword,
+  verifyToken,
+  verifyTokenId,
+};
